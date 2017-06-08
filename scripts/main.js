252508750ceef3e7e7ebbbbbf4ac274cb2f4d9b0
beeth0ven <beeth0vendev@163.com>
2017-06-08 22:39:26 +0800

    Add main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+
+var detailImageSelector = '[data-image-role="target"]';
+var detailTitleSelector = '[data-image-role="title"]';
+var thumbnailLinkSelector = '[data-image-role="trigger"]';
+
+var detailImage = document.querySelector(detailImageSelector);
