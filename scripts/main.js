5fd3e6804955f4a22f886009453e33056d2ec093
beeth0ven <beeth0vendev@163.com>
2017-06-10 22:47:14 +0800

    Add javascript

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,48 @@ var detailImageSelector = '[data-image-role="target"]';
 var detailTitleSelector = '[data-image-role="title"]';
 var thumbnailLinkSelector = '[data-image-role="trigger"]';
 
-var detailImage = document.querySelector(detailImageSelector);
+function setDetails(imageUrl, title) {
+  'use strict';
+  var detailImage = document.querySelector(detailImageSelector);
+  detailImage.src = imageUrl;
+
+  var detailTitle = document.querySelector(detailTitleSelector);
+  detailTitle.textContent = title;
+}
+
+function imageFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  "use strict";
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  "use strict";
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+  "use strict";
+  thumb.addEventListener('click', function (event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  })
+}
+
+function getThumbnails() {
+  "use strict";
+  var thumbnails = document.querySelectorAll(thumbnailLinkSelector);
+  return [].slice.call(thumbnails);
+}
+
+function initializeEvents() {
+  "use strict";
+  var thumbnails = getThumbnails();
+  thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
